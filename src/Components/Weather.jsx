0480fc48e5e3b2a11a669b8b3156604c91dbbb93
Hotaru24 <florent.pasquet.24@gmail.com>
2020-03-07 18:33:53 +0100

    metric convert

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-//const [data, setData] = useState(null);
+
+//const [icon, setIcon] = useState('');
+//const [layout, setLayout] = useState('');
+//const [temp, setTemp] = useState(0);
+//const [humidity, setHumidity] = useState(0);
+//const [widSpeed, setWindSpeed] = useState(0);
+//const [windDegree, setWindDegree] = useState(0);
 
 const  Weather = () => {
 
-
+const [name, setName] = useState('');
 const apiKey = "b2307c78a2534a93cbd29390deba0618";  
 
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition( (position) => {
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`)
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`)
     .then((result) => {
-      console.log(result)
+      setName : {position.coords.name}
     })}, console.log("err") );
   }) 
 
