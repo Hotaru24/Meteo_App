0a9f7a833a9b281044abe422e2a07ec59d70a65b
Hotaru24 <florent.pasquet.24@gmail.com>
2020-03-07 19:07:51 +0100

    basic jsx without css is ok

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 
-//const [icon, setIcon] = useState('');
-//const [layout, setLayout] = useState('');
-//const [temp, setTemp] = useState(0);
-//const [humidity, setHumidity] = useState(0);
-//const [widSpeed, setWindSpeed] = useState(0);
-//const [windDegree, setWindDegree] = useState(0);
+
 
 const  Weather = () => {
 
 const [name, setName] = useState('');
+const [icon, setIcon] = useState('');
+const [layout, setLayout] = useState('');
+const [temp, setTemp] = useState(0);
+const [humidity, setHumidity] = useState(0);
+const [widSpeed, setWindSpeed] = useState(0);
+const [windDegree, setWindDegree] = useState(0);
 const apiKey = "b2307c78a2534a93cbd29390deba0618";  
 
 
@@ -19,13 +20,24 @@ const apiKey = "b2307c78a2534a93cbd29390deba0618";
     navigator.geolocation.getCurrentPosition( (position) => {
     axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`)
     .then((result) => {
-      setName : {position.coords.name}
+      console.log(result.data)
+      setName(result.data.name);
+      setIcon(result.data.weather[0].icon);
+      setLayout(result.data.weather[0].description);
+      setTemp(result.data.main.temp);
+      setHumidity(result.data.main.humidity);
+      setWindSpeed(result.data.wind.speed);
+      setWindDegree(result.data.wind.deg)
     })}, console.log("err") );
   }) 
 
   return (
     <div>
-        <p>yo !</p>
+      <h2>{name}</h2>
+      <img src={icon} alt={layout}/>
+      <p>{temp}°C</p>
+      <p>Humidity : {humidity}%</p>
+      <p>Wind speed : {Math.round(widSpeed*3.6)} km/h</p>
     </div>
 
   )
