f418951b9dd73b3bc671f7adec1e7cce8ba9d4d7
Hotaru24 <florent.pasquet.24@gmail.com>
2020-03-07 18:09:34 +0100

    useEffet is ok

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-
-//const [,set] = useState();
-
-
+//const [data, setData] = useState(null);
 
 const  Weather = () => {
 
 
 const apiKey = "b2307c78a2534a93cbd29390deba0618";  
 
+
   useEffect(() => {
-    axios.get(`http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${apiKey}`)
+    navigator.geolocation.getCurrentPosition( (position) => {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`)
     .then((result) => {
-      setgameList(result.data)
-      console.log(result.data)
-    })
+      console.log(result)
+    })}, console.log("err") );
   }) 
 
   return (
-    <p>yo ! </p>
+    <div>
+        <p>yo !</p>
+    </div>
+
   )
 }
 
